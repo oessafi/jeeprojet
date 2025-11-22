dea5c62c3f0bed3a347142c902bff3cc87ffa887
oessafi <omaressafi772@gmail.com>
2025-11-22 20:04:05 +0100

    kafka

---
+++ notification-service/src/main/java/com/devbuild/notificationservice/consumer/NotificationConsumer.java
@@ -0,0 +1,19 @@
+package com.devbuild.notificationservice.consumer;
+
+import com.devbuild.notificationservice.dto.EmailRequest;
+import com.devbuild.notificationservice.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificationConsumer {
+
+    @Autowired
+    private EmailService emailService;
+
+    @KafkaListener(topics = "notification-topic", groupId = "notification-group")
+    public void consume(EmailRequest emailRequest) {
+        emailService.sendEmail(emailRequest.getTo(), emailRequest.getSubject(), emailRequest.getBody());
+    }
+}
---
+++ notification-service/src/main/java/com/devbuild/notificationservice/controller/NotificationController.java
@@ -0,0 +1,23 @@
+package com.devbuild.notificationservice.controller;
+
+import com.devbuild.notificationservice.dto.EmailRequest;
+import com.devbuild.notificationservice.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/notifications")
+public class NotificationController {
+
+    @Autowired
+    private EmailService emailService;
+
+    @PostMapping("/send-email")
+    public String sendEmail(@RequestBody EmailRequest emailRequest) {
+        emailService.sendEmail(emailRequest.getTo(), emailRequest.getSubject(), emailRequest.getBody());
+        return "Email sent successfully!";
+    }
+}
